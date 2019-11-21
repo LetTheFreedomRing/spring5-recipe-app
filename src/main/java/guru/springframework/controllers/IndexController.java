48e3fb9b13d834682b0bb127f2196648ef52961e
Pavlo Kolesnichenko <pavlo.kolesnichenko@gmail.com>
2019-11-21 14:27:25 +0200

    display recipes on index page

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,24 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.RecipeRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private final RecipeRepository recipeRepository;
+
+    public IndexController(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @RequestMapping({"/", "index", "index.html"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+
+        model.addAttribute("recipes", recipeRepository.findAll());
+
         return "index";
     }
 
